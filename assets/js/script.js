4bfa248d6227e80d23a8d8ff9b20d39ece2e9c7c
Carlo Gutiérrez <carlogtz@gmail.com>
2021-08-21 23:27:35 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(function(){
+    $(".botonEnviar").click(
+        function(){
+            alert("¡Muchas gracias! me pondré en contacto a la brevedad");
+        }
+    );      
+});
+/*$(function(){
+    $("a").click(function(event){
+        if (this.hash !== ""){
+            event.preventDefault();
+            var gato = this.hash;
+
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function(){
+                window.location.hash = gato;
+            });
+        }
+    });*/
